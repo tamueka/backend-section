b68bdf33de38109c1c899610455cc6609fb044d1
= <=>
2022-11-09 20:49:29 +0100

    [ADD] services added

---
+++ src/services/base.service.js
@@ -0,0 +1,69 @@
+class BaseService {
+  constructor(repository) {
+    this.repository = repository;
+  }
+
+  async get(id) {
+    if (!id) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "id must be send";
+      throw error;
+    }
+
+    if (!currentEntity) {
+      const error = new Error();
+      error.status = 404;
+      error.message = "entity not found";
+      throw error;
+    }
+
+    return await this.repository.get(id);
+  }
+
+  async getAll() {
+    return await this.repository.getAll();
+  }
+
+  async create(entity) {
+    if (!entity) {
+      const error = new Error();
+      error.status = 404;
+      error.message = "entity not found";
+      throw error;
+    }
+
+    return await this.repository.create(entity);
+  }
+
+  async update(id, entity) {
+    if (!id) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "id mus be send";
+      throw error;
+    }
+
+    if (!entity) {
+      const error = new Error();
+      error.status = 404;
+      error.message = "entity mus be send";
+      throw error;
+    }
+
+    return await this.repository.update(id, entity);
+  }
+
+  async delete(id) {
+    if (!id) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "id must be send";
+      throw error;
+    }
+
+    return await this.repository.delete(id);
+  }
+}
+
+module.exports = BaseService;
---
+++ src/services/user.service.js
@@ -0,0 +1,22 @@
+const BaseService = require("./base.service");
+let _userRepository = null;
+
+class UserService extends BaseService {
+  constructor({ userRepository }) {
+    super(userRepository);
+    _userRepository = userRepository;
+  }
+
+  async getUserByUsername(username) {
+    if (!username) {
+      const error = new Error();
+      error.status = 404;
+      error.message = "username not found";
+      throw error;
+    }
+
+    return await this._userRepository.getUserByUserName(username);
+  }
+}
+
+module.exports = UserService;
