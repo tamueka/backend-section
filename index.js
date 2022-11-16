dcea74008ac709e66414e7d5d438aaf15d71e499
= <=>
2022-11-16 21:02:18 +0100

    [FIX] fix problem mongoose connect MONGO URI

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const { MONGO_URI } = container.resolve("config");
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://tamueka:tamueka83@cluster0.926df.mongodb.net/shareyouridea?retryWrites=true&w=majority"
-  )
+  .connect(MONGO_URI)
   .then(() => server.start())
   .catch((err) => console.log("Error", err));
